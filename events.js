e3139a4e91f499a276a9099dfec87515a3537e28
Bhupendra Maurya <bhupendramaurya4050@gmail.com>
2024-11-10 23:00:12 +0530

    events_file_added

---
+++ events.js
@@ -0,0 +1,15 @@
+let button = document.querySelector("#id4");
+let currMode = "light";
+
+button.addEventListener("click", () => {
+    if(currMode === "light"){
+        currMode = "dark";
+        document.querySelector("body").style.backgroundColor = "black";
+    }
+    else{
+        currMode = "light";
+        document.querySelector("body").style.backgroundColor = "white";
+    }
+
+    console.log(currMode);
+});
