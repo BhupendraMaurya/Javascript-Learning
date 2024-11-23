3ca8ae1d7a7f8a429187441270c95fe082b50e65
Bhupendra Maurya <bhupendramaurya4050@gmail.com>
2024-11-23 17:19:24 +0530

    Async and await learning

--- async_await.js
+++ async_await.js
@@ -1,3 +1,42 @@
-async function hello(){
-    console.log("Hello");
+// Async function always returns a function compulsorily.
+// async function hello(){
+//     console.log("Hello");
+// }
+
+
+// function api(){
+//     return new Promise((resolve, reject)=>{
+//         setTimeout(() => {
+//             console.log("Weather data");
+//             resolve(20);
+//         },2000);
+//     })
+// }
+
+// async function getWeatherData(){
+//     await api();
+
+// }
+
+function getData(dataId){
+    return new Promise((resolve, reject) =>{
+        setTimeout(() =>{
+            console.log("data",dataId);
+            resolve("success");
+        },2000);
+    });
+}
+
+// Async - await
+
+async function getAllData(){
+    console.log("getting data 1");
+    await getData(1);
+    console.log("getting data 2");
+    await getData(2);
+    console.log("getting data 3");
+    await getData(3);
+    console.log("getting data 4");
+    await getData(4);
+
 }
