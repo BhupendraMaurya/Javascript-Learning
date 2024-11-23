7b2aa296e23e2b59bd5625013a5b60fc65bbe27a
Bhupendra Maurya <bhupendramaurya4050@gmail.com>
2024-11-23 11:22:12 +0530

    Promises intro

--- promises.js
+++ promises.js
@@ -58,3 +58,14 @@ let promise = new Promise((resolve, reject) => {
     // reject("Some error");
 })
 
+function getData(dataId, getNextData) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() =>{
+            console.log("data", dataId);
+            resolve("success");
+            if(getNextData){
+                getNextData();
+            }
+        },2000);
+    });
+}
