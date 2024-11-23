d4ad36baa605f4e83e85d2b600d3782390eaf412
Bhupendra Maurya <bhupendramaurya4050@gmail.com>
2024-11-23 16:17:06 +0530

    second example of promises

--- promises.js
+++ promises.js
@@ -72,58 +72,86 @@
 
 // 23-11-2024
 
-function asyncFunc(){
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log("Some data1");
-            resolve("Success");
-        },4000);
+// function asyncFunc(){
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("Some data1");
+//             resolve("Success");
+//         },4000);
+//     });
+// }
+
+// function asyncFunc2(){
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("Some data of second function");
+//             resolve("second function resolved successfully");
+//         },4000);
+//     });
+// }
+
+// function asyncFunc3(){
+//     return new Promise((resolve,reject) =>{
+//         setTimeout(() => {
+//             console.log("third promise is getting ready");
+//             //resolve("Third promise would be resolved here");
+//             reject("Third promise did not execute");
+//         },5000);
+//     })
+// }
+
+// let p1 = asyncFunc();
+// p1.then((res) => {
+//     console.log("hello promises");
+//     console.log(res);
+
+//     // promises chaining;
+//     // so here firstly first promises would be executed and then only second promise would be 
+//     // executed.
+//     let p2 = asyncFunc2();
+//     p2.then((res) => {
+//         console.log("second promise is executed");
+//         console.log(res);
+
+//         // promise chaining for 3rd function
+//         asyncFunc3().then((res) => {
+//             console.log("Third promise is exectuing");
+//             console.log(res);
+//         });
+
+//         asyncFunc3().catch((err) => {
+//             console.log("third func did not execute so catching its error");
+//             console.log(err);
+//         })
+//     });
+// });
+
+
+
+// 2nd example of promise chain
+console.log("2nd example of promise chaining");
+function getData(dataId){
+    return new Promise((resolve, reject) =>{
+        setTimeout(() =>{
+            console.log("data", dataId);
+            resolve("success");
+        },3000);
     });
 }
 
-function asyncFunc2(){
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log("Some data of second function");
-            resolve("second function resolved successfully");
-        },4000);
-    });
-}
-
-function asyncFunc3(){
-    return new Promise((resolve,reject) =>{
-        setTimeout(() => {
-            console.log("third promise is getting ready");
-            //resolve("Third promise would be resolved here");
-            reject("Third promise did not execute");
-        },5000);
-    })
-}
-
-let p1 = asyncFunc();
-p1.then((res) => {
-    console.log("hello promises");
+// now executing the promise chain
+console.log("Fetching the first data");
+getData(1).then((res) =>{
     console.log(res);
 
-    // promises chaining;
-    // so here firstly first promises would be executed and then only second promise would be 
-    // executed.
-    let p2 = asyncFunc2();
-    p2.then((res) => {
-        console.log("second promise is executed");
+    console.log("Fetching the 2nd data");
+    getData(2).then((res) => {
         console.log(res);
 
-        // promise chaining for 3rd function
-        asyncFunc3().then((res) => {
-            console.log("Third promise is exectuing");
+        console.log("Fetching the 3rd data");
+        getData(3).then((res) => {
             console.log(res);
-        });
-
-        asyncFunc3().catch((err) => {
-            console.log("third func did not execute so catching its error");
-            console.log(err);
         })
-    });
-});
+    })
 
-
+})
