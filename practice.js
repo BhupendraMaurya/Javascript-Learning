701a16a4af68aa82b4d17cdcae63b2ff0135dfa1
Bhupendra Maurya <bhupendramaurya4050@gmail.com>
2024-12-13 23:17:47 +0530

    Basic array and array transformation questions

--- practice.js
+++ practice.js
@@ -254,44 +254,189 @@
 // console.log(hello("how are you", "Dharmendra"));
 
 // Closures
-function closure(){
-    let x = 56;
-    function add(){
-        x = x + 45;
-        console.log(x);
+// function closure(){
+//     let x = 56;
+//     function add(){
+//         x = x + 45;
+//         console.log(x);
+//     }
+
+//     return add;
+// }
+
+// let value = closure();
+// value();
+
+// value();
+
+// function createCounter(){
+//     let count = 0;
+
+//     return function (){
+//         count++;
+//         return count;
+//     };
+// }
+
+// const counter = createCounter();
+// console.log(counter());
+// console.log(counter());
+
+
+// function closure (){
+//     let count = 0;
+//     function hello(){
+//         console.log("Hello private bro");
+//     }
+
+//     hello();
+// }
+
+// console.log(count)
+
+// closure();
+
+// function callback(){
+//     console.log("I am callback");
+// // }
+
+// function callback(){
+//     console.log("I am callback");
+// }
+
+// function hello(callback){
+//     console.log("BEfore callback:");
+//     callback();
+//     console.log("After callback");
+// }
+
+// hello(callback());
+
+// Arrays
+
+// const arr = [1,2,3,4,5];
+
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+
+// let n = arr.length;
+
+// console.log(arr[0] +" "+ arr[n-1])
+// arr.push(6);
+// console.log(arr);
+
+// arr.unshift(7);
+// console.log(arr);
+
+// console.log(arr.shift());
+
+// arr.pop();
+// console.log(arr);
+
+// console.log(arr.length);
+
+// let arr = [1,2,3,4,5,6]
+// arr.forEach(function print(val){
+//     console.log(val);
+// })
+// console.log("\n")
+// function print(val){
+//     console.log(val);
+// }
+
+// arr.forEach(function print(val){
+//     console.log(val);
+// });
+
+// arr.forEach((val) =>{
+//     console.log(val);
+// })
+
+// let arr = [1,2,4,5,6,7];
+// console.log(arr);
+
+// function doubled(x){
+//     return 2 * x;
+// }
+
+// let double = arr.map(doubled);
+// console.log(double);
+
+// let double2 = arr.map((x) => {return 3*x} );
+// console.log(double2);
+
+// let triple = arr.map(function tripled(x){
+//     return 4 * x;
+// })
+
+// console.log(triple); 
+
+// let arr = [1,2,3,4,5,6,6,7]
+// console.log(arr);
+
+// function newArray(x){
+//     return x % 2;
+// }
+
+// let ans = arr.filter(newArray);
+// console.log(ans);
+
+let arr = [0,1,2,-300,4,5,6000];
+console.log(arr);
+
+/////////////////
+console.log("Finding the sum using for loop");
+
+function addition(arr){
+    let sum = 0;
+    
+    for(let i=0;i<arr.length;i++){
+        sum += arr[i];
     }
 
-    return add;
+    return sum;
 }
 
-let value = closure();
-value();
-
-value();
-
-function createCounter(){
-    let count = 0;
-
-    return function (){
-        count++;
-        return count;
-    };
-}
-
-const counter = createCounter();
-console.log(counter());
-console.log(counter());
+let ans = addition(arr);
+console.log(ans);
 
 
-function closure (){
-    let count = 0;
-    function hello(){
-        console.log("Hello private bro");
+//////////////
+console.log("Finding the sum using the Reduce method");
+
+// another way to add the elements of given array
+let sum = arr.reduce((acc, curr) => {
+    acc += curr;
+    return acc;
+},0);
+
+console.log(sum);
+
+///////////////
+console.log("Finding the min value from array using the Reduce()");
+let mini = arr.reduce((acc, curr) =>{
+    if(curr < acc){
+        acc = curr;
+        return acc;
     }
+    else{
+        return acc;
+    }
+},Number.MAX_VALUE);
 
-    hello();
-}
+console.log(mini);
 
-console.log(count)
+///////////////
+console.log("Finding the maximum value using the reduce()");
+let maxi = arr.reduce((acc, curr) => {
+    if(curr > acc){
+        acc = curr;
+        return acc;
+    }
+    else{
+        return acc;
+    }
+}, Number.MIN_VALUE);
 
-closure();
+console.log(maxi);
