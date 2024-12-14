e53c3e8a5662fb4c646782d08c7b5eb496290cea
Bhupendra Maurya <bhupendramaurya4050@gmail.com>
2024-12-14 18:07:34 +0530

    Searching and Sorting in Arrays

--- arrays2.js
+++ arrays2.js
@@ -72,57 +72,139 @@
 // console.log(arr);
 
 /////////////////
-console.log("Finding the sum using for loop");
+// console.log("Finding the sum using for loop");
 
-function addition(arr){
-    let sum = 0;
+// function addition(arr){
+//     let sum = 0;
     
-    for(let i=0;i<arr.length;i++){
-        sum += arr[i];
-    }
+//     for(let i=0;i<arr.length;i++){
+//         sum += arr[i];
+//     }
 
-    return sum;
+//     return sum;
+// }
+
+// let ans = addition(arr);
+// console.log(ans);
+
+
+// //////////////
+// console.log("Finding the sum using the Reduce method");
+
+// // another way to add the elements of given array
+// let sum = arr.reduce((acc, curr) => {
+//     acc += curr;
+//     return acc;
+// },0);
+
+// console.log(sum);
+
+// ///////////////
+// console.log("Finding the min value from array using the Reduce()");
+// let mini = arr.reduce((acc, curr) =>{
+//     if(curr < acc){
+//         acc = curr;
+//         return acc;
+//     }
+//     else{
+//         return acc;
+//     }
+// },Number.MAX_VALUE);
+
+// console.log(mini);
+
+// ///////////////
+// console.log("Finding the maximum value using the reduce()");
+// let maxi = arr.reduce((acc, curr) => {
+//     if(curr > acc){
+//         acc = curr;
+//         return acc;
+//     }
+//     else{
+//         return acc;
+//     }
+// }, Number.MIN_VALUE);
+
+// console.log(maxi);
+
+const arr = [1,2,3,4,5,6, 45, 56]
+console.log(arr)
+
+// finding the index of array element 2
+console.log(arr.indexOf(2));
+
+// checking the existent of any value in array'
+
+function exist(x){
+    if(arr.indexOf(x) ==-1){
+        console.log(`${x} does not exist in array`);
+    }
+    else{
+        console.log(`${x} exist in given array`);
+    }
 }
 
-let ans = addition(arr);
-console.log(ans);
+exist(2);
+exist(-5);
 
+// checking the existent of any number using the includes
 
-//////////////
-console.log("Finding the sum using the Reduce method");
+console.log(arr.includes(2)) // search for 2 from starting
+console.log(arr.includes(-10, 2)) // start searching for 2, from index 2.
 
-// another way to add the elements of given array
-let sum = arr.reduce((acc, curr) => {
-    acc += curr;
-    return acc;
-},0);
+// Find the first number greater than 10 in an array using find.
+// using the for loop
 
-console.log(sum);
-
-///////////////
-console.log("Finding the min value from array using the Reduce()");
-let mini = arr.reduce((acc, curr) =>{
-    if(curr < acc){
-        acc = curr;
-        return acc;
+for(let i=0;i<arr.length;i++){
+    if(arr[i] > 10){
+        console.log(arr[i]);
+        break;
     }
-    else{
-        return acc;
-    }
-},Number.MAX_VALUE);
+}
 
-console.log(mini);
+// using the find()
+let ans2 = arr.find(curr => curr > 10);
+console.log(ans2);
 
-///////////////
-console.log("Finding the maximum value using the reduce()");
-let maxi = arr.reduce((acc, curr) => {
-    if(curr > acc){
-        acc = curr;
-        return acc;
-    }
-    else{
-        return acc;
-    }
-}, Number.MIN_VALUE);
+let ans3 = arr.find(function hello(curr){
+    return curr > 100;
+});
+console.log(ans3);
 
-console.log(maxi);
+// Find all numbers greater than 10 using filter.
+
+// without using the filter()
+let arr2 = []
+for(let i=0;i<arr.length;i++){
+    if(arr[i] > 10){
+        arr2.push(arr[i]);
+    }
+}
+console.log(arr2);
+
+// using the filter();
+
+function greaterThanTen(x){
+    return x > 10;
+}
+
+let result = arr.filter(greaterThanTen)
+console.log(result)
+
+// using the arrow function for the above function
+let result2 = arr.filter((x) => x > 10)
+console.log(result2);
+
+
+// Sort an array of numbers in ascending and descending order.
+let number = [1,4,2,5,2,3,7,5];
+console.log(number)
+// custom logic to sort in ascending order
+number.sort((a,b) => a - b);
+
+console.log(number);
+
+// logic to sort in descending order
+number.sort((a,b) => b - a);
+console.log(number);
+
